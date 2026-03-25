59f7c226a205412498bf0b4c1c983b901b5cf9ac
krita-voi <kritagyamaharjan794@gmail.com>
2026-03-25 14:06:22 +0545

    movie application

--- movies-app/src/pages/Home.jsx
+++ movies-app/src/pages/Home.jsx
@@ -1,21 +1,35 @@
+import "../css/Home.css";
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "O Captain", release_date: "1989" },
     { id: 2, title: "Terminater", release_date: "2010" },
   ];
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("------");
+  };
 
   return (
     <div className="home">
-      <form onSubmit={handleSearch} className="searh-form">
+      <form onSubmit={handleSearch} className="search-form">
         <input
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
+
       <div className="movies-gird">
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
